b94fe0f38bc79d4a62f4a049e518fe6f646086a2
Bruno Kawakami <bru.kawakami@gmail.com>
2013-03-01 09:47:43 -0300

    Interface de formularios finalzada, @TODO configurar layout

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,11 +11,14 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Form\IndexForm;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $form = new IndexForm;
+        
+        return array('form'=>$form);
     }
 }
--- module/Application/src/Application/Form/IndexForm.php
+++ module/Application/src/Application/Form/IndexForm.php
@@ -7,38 +7,89 @@ use Zend\Form\Form;
 class IndexForm extends Form {
 
     public function __construct($name = null) {
-        
+
         parent::__construct('index');
         $this->setAttribute('method', 'post');
         $this->add(array(
-            'name' => 'id',
-            'attributes' => array(
-                'type' => 'hidden',
-            ),
-        ));
-        $this->add(array(
-            'name' => 'title',
+            'name' => 'os',
             'attributes' => array(
                 'type' => 'text',
             ),
             'options' => array(
-                'label' => 'Title',
+                'label' => 'Número da OS',
             ),
         ));
         $this->add(array(
-            'name' => 'artist',
+            'name' => 'serie',
             'attributes' => array(
                 'type' => 'text',
             ),
             'options' => array(
-                'label' => 'Artist',
+                'label' => 'Numero de série',
             ),
         ));
         $this->add(array(
-            'name' => 'submit',
+            'name' => 'nf',
+            'attributes' => array(
+                'type' => 'text',
+            ),
+            'options' => array(
+                'label' => 'NF. Compra',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'cpf',
+            'attributes' => array(
+                'type' => 'text',
+            ),
+            'options' => array(
+                'label' => 'CPF Consumidor',
+            ),
+        ));
+        
+        $this->add(array(
+            'type' => 'select',
+            'name' => 'tipo_os',
+            'options' => array(
+                'label' => 'Tipo OS',
+                'value_options' => array(
+                    '0' => 'Todas',
+                    '1' => 'Consumidor',
+                    '2' => 'Revenda',
+                ),
+            )
+        ));
+        $this->add(array(
+            'type' => 'select',
+            'name' => 'tipo_atendimento',
+            'options' => array(
+                'label' => 'Tipo Atendimento',
+                'value_options' => array(
+                    '0' => 'Atendimento balcão',
+                    '1' => 'Atendimento com deslocamento',
+                ),
+            )
+        ));
+        $this->add(array(
+            'type' => 'select',
+            'name' => 'status_os',
+            'options' => array(
+                'label' => 'Status de OS',
+                'value_options' => array(
+                    '0' => 'Aberta Call-Center',
+                    '1' => 'Aguardando Análise',
+                    '2' => 'Aguardando Peça',
+                    '3' => 'Aguardando Conserto',
+                    '4' => 'Aguardando Retirada',
+                    '5' => 'Aguardando Finalizada',
+                ),
+            )
+        ));
+        $this->add(array(
+            'name' => 'submit_1',
             'attributes' => array(
                 'type' => 'submit',
-                'value' => 'Go',
+                'value' => 'Pesquisar',
                 'id' => 'submitbutton',
             ),
         ));
